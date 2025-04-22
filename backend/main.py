1c19cf5fb7ba28b11cdb5b3bcafd428026f407e4
L Dettling <tdettling@tds.net>
2025-04-21 23:42:01 -0400

    heroku backend deployment

--- backend/main.py
+++ backend/main.py
@@ -511,3 +511,6 @@ def delete_ISD(isd_number: int, db: Session = Depends(database.get_db),
 
 
 
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
