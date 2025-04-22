8894a710472f8ea753d1d2e20d4337c3b11063b3
L Dettling <tdettling@tds.net>
2025-04-22 00:12:22 -0400

    more database chnages

--- backend/database.py
+++ backend/database.py
@@ -1,21 +1,3 @@
-'''
-L Dettling
-CIS 658
-
-Sources: 
-https://docs.sqlalchemy.org/en/20/core/engines.html
-https://www.geeksforgeeks.org/connecting-to-sql-database-using-sqlalchemy-in-python/
-https://docs.oracle.com/en/cloud/paas/autonomous-database/serverless/adbsb/connecting-python-mtls.html
-https://docs.oracle.com/en/cloud/paas/autonomous-database/serverless/adbsb/connecting-python-mtls.html#GUID-70853600-E169-4C46-909C-39E9DE443850
-https://www.oracle.com/a/ocom/docs/database/adb-ssis-sdt.pdf
-
-https://www.youtube.com/watch?v=MWrD-99xqzs
-https://www.youtube.com/watch?v=BCLsb-AdrhQ
-https://www.youtube.com/watch?v=YnsN52hB8EY
-
-'''
-
-
 import oracledb
 import app_settings
 from sqlalchemy import create_engine
@@ -28,22 +10,16 @@ Base = declarative_base()
 # Load app settings
 settings = app_settings.get_settings()
 
-CONFIG_DIR = r"C:\Users\tdett\OracleDB"  
-WALLET_DIR = r"C:\Users\tdett\OracleDB"  
-
 oracle_username = settings.ORACLE_DB_USERNAME
 oracle_password = settings.ORACLE_DB_PASSWORD
-oracle_wallet_password = settings.ORACLE_WALLET_PASSWORD
 
+# Thin mode init (optional, but safe to include)
 oracledb.init_oracle_client(lib_dir=None)
 
-DATABASE_URL = (
-    f"oracle+oracledb://{oracle_username}:{oracle_password}@zjerw393z01twmro_low?mode=thin"
-)
-
+# Ensure you use mode=thin
+DATABASE_URL = f"oracle+oracledb://{oracle_username}:{oracle_password}@zjerw393z01twmro_low?mode=thin"
 
 engine = create_engine(DATABASE_URL, pool_size=5, max_overflow=10)
-
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 def get_db():
